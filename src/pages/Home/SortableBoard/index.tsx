02cf97fd68bbe0125f90b35e399a7bece5f098d0
youhei usami <usao9110@icloud.com>
2025-07-11 18:23:44 +0400

    7/11

--- src/pages/Home/SortableBoard/index.tsx
+++ src/pages/Home/SortableBoard/index.tsx
@@ -1,22 +1,28 @@
 import { SortableList } from './SortableList';
 import { AddList } from './AddList';
-import {  useAtomValue } from "jotai";
+import { useAtom, useAtomValue } from "jotai";
 import { currentUserAtom } from "../../../modules/auth/current-user.state";
 import { listRepository } from "../../../modules/lists/list.repository";
+import { listsAtom } from "../../../modules/lists/list.state";
 
 export default function SortableBoard() {
   const currentUser = useAtomValue(currentUserAtom);
+  const [lists, setLists] = useAtom(listsAtom);
+  const sortedLists = [...lists].sort((a, b) => a.position - b.position)
 
   const createList = async (title: string) => {
     const newList = await listRepository.create(currentUser!.boardId, title);
-    console.log(newList);
+    setLists((prevLists) => [...prevLists, newList]);
+    setLists([...lists, newList])
     
   }
 
   return (
     <div className="board-container">
       <div style={{ display: 'flex', gap: '12px' }}>
-        <SortableList />
+        {sortedLists.map((list) => (
+          <SortableList list={list}/>
+        ))}
       </div>
       <AddList onCreate={createList}/>
     </div>
