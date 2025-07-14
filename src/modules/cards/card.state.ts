dee70161b05e5c48f68bbab324cbb474f26af0f2
youhei usami <usao9110@icloud.com>
2025-07-14 11:54:08 +0400

    7/14

--- src/modules/cards/card.state.ts
+++ src/modules/cards/card.state.ts
@@ -2,4 +2,14 @@ import { atom } from "jotai";
 import { Card } from "./card.entity";
 
 
-export const cardsAtom = atom<Card[]>([])
+export const cardsAtom = atom<Card[]>([]);
+
+export const selectedCardIdAtom = atom<string | null>(null);
+
+export const selectedCardAtom = atom((get) => {
+  const selectedCardId = get(selectedCardIdAtom);
+  const cards = get(cardsAtom);
+  return selectedCardId != null
+  ? cards.find((card) => card.id == selectedCardId)
+  : null;
+})
